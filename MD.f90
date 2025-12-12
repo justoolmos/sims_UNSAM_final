7ac6404fddc3f963d390500266a952fe93580d56
justo.olmos <justo.olmos388@gmail.com>
2025-12-12 20:23:16 -0300

    paralelo primer intento

--- MD.f90
+++ MD.f90
@@ -15,56 +15,78 @@ subroutine init_coords()
         end do
 end subroutine init_coords
 
-function randn() result(z)
-    implicit none
-    real(kind=8) :: z
-    real(kind=8) :: u1, u2
-
-    call random_number(u1)
-    call random_number(u2)
-
-    ! Avoid log(0)
-    if (u1 == 0d0) u1 = 1d-16
-
-    z = sqrt(-2d0*log(u1)) * cos(2d0*acos(-1d0) * u2)
-end function randn
-
 subroutine update_E_and_F() 
-        integer :: i,j
-        real(kind=8) :: r_ij(3), norm, ex, v_ij, p
-        p = 0.0 
-        E_tot = 0.0
-        F = 0.0
-        do i=1,N-1
-                do j=i+1,N
-                        r_ij = r(:,i)-r(:,j)
-                        norm = sqrt(sum(r_ij**2))
+    implicit none 
+    integer :: i,j,k,n
+    real(kind=8) :: r_ij(3), norm2, ex, ex2, v_ij, p, F_esc
+    real(kind=8) :: ex_cut, F_cut, delta, frac_L, Ldiv2
+    real(kind=8) :: sigma6, c24eps, L2_4, rcut2, norm2_3
 
-                        !corregimos distancia a la imagen mas cercana
-                        if(norm > L/2) then
-                                r_ij = r_ij - (L * int(2*r_ij/L))
-                                norm = sqrt(sum(r_ij**2))
-                        end if
+    ! Inicializaciones
+    p = 0.0
+    E_tot = 0.0
+    F = 0.0
 
-                        !computamos fuerzas y energia solo si la distancia es menor a r_cut
-                        if(norm < r_cut) then
-                                ex = (sigma/norm)**6
-                                v_ij = 4.0*eps*(-ex+(ex*ex)) - v_cut
-                                E_tot = E_tot + v_ij
-                                
-                                ex_cut = (sigma/r_cut)**6
-                                F_cut = 24.0*eps*(ex_cut-2.0*ex_cut*ex_cut)/(r_cut*r_cut)
-                                F_esc = 24.0*eps*(ex-2.0*ex*ex)/(norm*norm) - F_cut
-                                F(:,i) = F(:,i) - F_esc*r_ij
-                                F(:,j) = F(:,j) + F_esc*r_ij
-                                !F(:,i) = F(:,i)-(r(:,i)*(4.0*eps*(6*ex-12*ex*ex))/norm**2)
-                                !F(:,j) = F(:,j)-(r(:,j)*(4.0*eps*(6*ex-12*ex*ex))/norm**2)
-                                p = p + dot_product(r_ij,-F_esc*r_ij)
-                        end if
-                end do
+    ! Cálculos fuera del loop
+    sigma6 = sigma**6
+    c24eps = 24.0*eps
+    L2_4   = L*L/4
+    frac_L = 2.0/L
+    Ldiv2 = L/2.0
+    rcut2  = r_cut*r_cut
+    ex_cut = (sigma/r_cut)**6
+    F_cut  = c24eps*(ex_cut - 2.0*ex_cut*ex_cut)/rcut2
+
+    !$OMP PARALLEL PRIVATE(i,j,k,r_ij,norm2,ex,ex2,v_ij,delta,norm2_3,F_esc) REDUCTION(+:p, E_tot)
+    ! Loop sobre pares de partículas
+    !$OMP DO SCHEDULE(STATIC,2)
+    do i=1,N-1
+        do j=i+1,N
+            ! Diferencia de posición
+            !!r_ij = r(:,i) - r(:,j)
+            !norm2 = r_ij(1)*r_ij(1) + r_ij(2)*r_ij(2) + r_ij(3)*r_ij(3)
+
+            ! Corrección de imágenes periódicas
+            !if(norm2 > L2_4) then
+            !    r_ij = r_ij - (L * int(2*r_ij/L))
+            !    norm2 = r_ij(1)*r_ij(1) + r_ij(2)*r_ij(2) + r_ij(3)*r_ij(3)
+            !end if
+            !!r_ij = r_ij - L * nint(2*r_ij/L)
+            
+            !se hace la correccion de imagen periodica elemento a elemento y luego se calcula norm2, para calcularlo una sola vez
+            do k = 1,3
+                delta = r(k,i) - r(k,j)
+                ! Aplicar PBC solo si es necesario
+                if(abs(delta) > Ldiv2) then
+                    n = nint(delta*frac_L)
+                    delta = delta - n*L
+                end if
+                r_ij(k) = delta
+            end do
+            norm2 = r_ij(1)*r_ij(1) + r_ij(2)*r_ij(2) + r_ij(3)*r_ij(3)
+
+            ! Computar fuerzas y energía si dentro de r_cut
+            if(norm2 < rcut2) then
+                ! Evitar potencia usando multiplicaciones
+                norm2_3 = norm2*norm2*norm2
+                ex   = sigma6 / norm2_3
+                ex2 = ex*ex
+                v_ij = 4.0*eps*(-ex + ex2)
+                v_ij = v_ij - v_cut
+                E_tot = E_tot + v_ij
+
+                F_esc = c24eps*(ex - 2.0*ex2)/norm2 - F_cut
+                F(:,i) = F(:,i) - F_esc*r_ij
+                F(:,j) = F(:,j) + F_esc*r_ij
+
+                p = p  -F_esc * norm2
+                !p = p + dot_product(r_ij, -F_esc*r_ij)
+            end if
         end do
-        p_inst = N*get_T()/(L**3) + 1.0/(3*L**3)*p
-
+    end do
+   !$OMP END DO
+   !$OMP END PARALLEL
+    p_inst = N*get_T()/(L**3) + 1.0/(3*L**3)*p
 end subroutine update_E_and_F
 
 subroutine update_lgv_F()
@@ -125,9 +147,8 @@ end subroutine save_coords
 
 
 subroutine integrate() 
-
         !asume que la fuerza del paso anterior ya fue calculada
-        r = r + v*dt + 0.5*(F/m)*dt**2
+        r = r + v*dt + 0.5*(F/m)*dt**2  !revisar si esto es paralelizable
         call pbc()
         v = v + 0.5*F*dt/m
         call update_E_and_F()
